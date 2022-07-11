d11ae0dc303466bdf483349dea6e1180959d2ca9
diorlitao <diorlitao@gmail.com>
2022-07-11 14:11:00 +0800

    '自动定时提交测试'

---
+++ autoCommit.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const fs = require('fs');
+const simpleGit = require('simple-git');
+
+// 定时器
+setInterval(function () {
+  upDataFile();
+}, 1000 * 60 * 60 * 24); //时间不易太短
+
+// 修改 README 文件
+upDataFile();
+function upDataFile() {
+  const time = Date();
+  fs.appendFile(__dirname + '/README.md', '#### 自动 commit，时间:' + time + '\r\n', (err) => {
+    err ? console.error('缺少 README.md ') : console.log('README 文件追加成功，时间: ' + time);
+    gitCommit(time);
+  });
+}
+
+// commit 提交
+function gitCommit(time) {
+  simpleGit()
+    .addConfig('user.name', 'liangtongzhuo')
+    .addConfig('user.email', 'liangtongzhuo@gmail.com')
+    .add('./*')
+    .commit('自动 commit，时间' + time)
+    .push(['-u', 'origin', 'master'], (e) => {
+      console.log('commit 成功，时间：' + time, e);
+    });
+}
